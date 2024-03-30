cee077a713d98743cfb0cd62402099c59ec8e4e9
prabath1998 <prabhathudayanga1998@gmail.com>
2024-03-30 14:27:13 +0530

    minor ui improvements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,14 +15,14 @@
         @csrf
 
         @if (session('success'))
-            <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+            <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-100  dark:text-green-400"
                 role="alert">
                 <span class="font-medium">{{ session('success') }}
             </div>
         @endif
 
         @if (session('error'))
-            <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+            <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100 dark:text-red-400"
                 role="alert">
                 <span class="font-medium">{{ session('error') }}
             </div>
