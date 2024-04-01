41d8e8a90fe57a7dae7902d8a7851092ad94c983
prabath1998 <prabhathudayanga1998@gmail.com>
2024-04-01 16:57:16 +0530

    Fix minor bugs

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -20,48 +20,97 @@ class DebugController extends Controller
     }
 
     // Check if the code has a bracket or quote mismatching error
+    // public function checkSyntax($data)
+    // {
+
+    //     $stack = [];
+    //     $openBrackets = ['(', '{', '['];
+    //     $closeBrackets = [')', '}', ']'];
+    //     $bracketsMap = [')' => '(', '}' => '{', ']' => '['];
+
+    //     $string = preg_replace('/\s+/', '', $data);
+
+    //     $doubleQuoteCount = substr_count($string, '"');
+    //     $singleQuoteCount = substr_count($string, "'");
+
+    //     if ($doubleQuoteCount % 2 != 0 || $singleQuoteCount % 2 != 0) {
+    //         return false;
+    //     }
+
+    //     try {
+    //         for ($i = 0; $i < strlen($string); $i++) {
+    //             $char = $string[$i];
+
+    //             if (in_array($char, $openBrackets)) {
+    //                 array_push($stack, $char);
+    //             } elseif (in_array($char, $closeBrackets)) {
+    //                 if (empty($stack)) {
+    //                     return false;
+    //                 }
+    //                 $top = array_pop($stack);
+    //                 if ($bracketsMap[$char] !== $top) {
+    //                     return false;
+    //                 }
+    //             }
+    //         }
+
+    //         if (empty($stack)) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     } catch (\Exception $e) {
+    //         Log::error($e->getMessage());
+    //         return false;
+    //     }
+    // }
     public function checkSyntax($data)
     {
-
         $stack = [];
         $openBrackets = ['(', '{', '['];
         $closeBrackets = [')', '}', ']'];
         $bracketsMap = [')' => '(', '}' => '{', ']' => '['];
+        $escaped = false;
 
-        $string = preg_replace('/\s+/', '', $data);
 
-        $doubleQuoteCount = substr_count($string, '"');
-        $singleQuoteCount = substr_count($string, "'");
+        for ($i = 0; $i < strlen($data); $i++) {
+            $char = $data[$i];
 
-        if ($doubleQuoteCount % 2 != 0 || $singleQuoteCount % 2 != 0) {
-            return false;
-        }
+            if ($escaped) {
+                $escaped = false;
+                continue;
+            }
 
-        try {
-            for ($i = 0; $i < strlen($string); $i++) {
-                $char = $string[$i];
+            if ($char === '\\') {
+                $escaped = true;
+                continue;
+            }
 
-                if (in_array($char, $openBrackets)) {
-                    array_push($stack, $char);
-                } elseif (in_array($char, $closeBrackets)) {
-                    if (empty($stack)) {
-                        return false;
-                    }
-                    $top = array_pop($stack);
-                    if ($bracketsMap[$char] !== $top) {
-                        return false;
-                    }
+            // Handle quotes
+            if ($char === '"' || $char === "'") {
+                $quote = $char;
+                $endPos = strpos($data, $quote, $i + 1);
+                if ($endPos === false) {
+                    return false;
+                }
+                $i = $endPos;
+                continue;
+            }
+
+            // Handle brackets
+            if (in_array($char, $openBrackets)) {
+                array_push($stack, $char);
+            } elseif (in_array($char, $closeBrackets)) {
+                if (empty($stack)) {
+                    return false;
+                }
+                $top = array_pop($stack);
+                if ($bracketsMap[$char] !== $top) {
+                    return false;
                 }
             }
-
-            if (empty($stack)) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-            return false;
         }
+
+        return empty($stack); 
     }
 }
